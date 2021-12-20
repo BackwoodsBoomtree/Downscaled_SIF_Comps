7eb23eb5eb34bd1b2b353f906eea1258d9539876
Doughty <russell.doughty@ou.edu>
2021-12-20 13:07:26 -0600

    Add dates

--- prep_gosif.R
+++ prep_gosif.R
@@ -14,6 +14,14 @@ gosif[gosif >= 32766] <- NA
 # Scale
 gosif <- gosif * 0.0001
 
+# Times
+date_list <- seq(as.Date("2020-01-01"), as.Date("2020-12-31"), by = 8)
+
+for (t in 1:46) {
+  time(gosif[[t]]) <- date_list[t]
+}
+
+# Write out
 writeCDF(gosif, paste0(out_dir, "/GOSIF.2020.5km.nc"),
          varname = "gosif", longname = "GOSIF 8-day Mean", unit = "mW/m-2/sr/nm",
          missval = -9999, overwrite = TRUE, compression = 4)
