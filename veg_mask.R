a0ec3966021c6eb952afdddf60d381278ce14084
Doughty <russell.doughty@ou.edu>
2022-04-10 23:01:15 -0500

    Make more conservative veg mask at NDVI 0.4

--- veg_mask.R
+++ veg_mask.R
@@ -7,12 +7,12 @@ ndvi_mask <- rast("G:/TROPOMI/esa/gridded/1deg/monthly/2021/TROPOMI.ESA.SIF.2021
 
 # Get max annual values for masking
 ndvi_mask <- max(ndvi_mask, na.rm = TRUE)
-ndvi_mask[ndvi_mask < 0.2] <- NA
+ndvi_mask[ndvi_mask < 0.4] <- NA
 
 plot(ndvi_mask, col = viridis(100))
 
-ndvi_mask[ndvi_mask >= 0.2] <- 1
+ndvi_mask[ndvi_mask >= 0.4] <- 1
 
 plot(ndvi_mask)
 
-writeRaster(ndvi_mask, "G:/SIF_comps/veg_mask/max.monthly.ndvi.1deg.tif", overwrite = TRUE)
+writeRaster(ndvi_mask, "G:/SIF_comps/veg_mask/max.monthly.ndvi.04.1deg.tif", overwrite = TRUE)
