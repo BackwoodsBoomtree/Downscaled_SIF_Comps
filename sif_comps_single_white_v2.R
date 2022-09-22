e9f29453a888a293f49fddec9caf0ac6d5242e5b
Doughty <russell.doughty@ou.edu>
2022-09-22 17:40:22 -0500

    Update for new data

--- sif_comps_single_white_v2.R
+++ sif_comps_single_white_v2.R
@@ -1,13 +1,14 @@
 library(terra)
 library(ncdf4)
 library(viridis)
+library(corrplot)
 
 out_name    <- "G:/SIF_comps/figs/v2/sif_comps_single_white_v2.pdf"
 
 # SIF data
 tropo_sif_data  <- read.csv("G:/SIF_comps/csv/tropomi/Amazon_TROPOSIF_L2B_2019-2021_sifd_mean.csv", header = TRUE)
-oco2_sif_data   <- read.csv("G:/SIF_comps/csv/oco2/Amazon_OCO2_L2B10_2015-2021_sifd_mean_qc_nadir.csv", header = TRUE)
-oco3_sif_data   <- read.csv("G:/SIF_comps/csv/oco3/Amazon_OCO3_L2B10_2019-2022_sifd_mean_qc_nadir.csv", header = TRUE)
+oco2_sif_data   <- read.csv("G:/SIF_comps/csv/oco2/Amazon_OCO2_L2B11_2019-2022_sifd_740_mean_qc_nadir.csv", header = TRUE)
+oco3_sif_data   <- read.csv("G:/SIF_comps/csv/oco3/Amazon_OCO3_L2B10_2019-2022_sifd_740_mean_qc_nadir.csv", header = TRUE)
 gome2_sif_data  <- read.csv("G:/SIF_comps/csv/gome2/Amazon_NSIFv2.6.2.GOME-2A_2011-2018_sifd_mean_qc2.csv", header = TRUE)
 
 tropo_sif_mean <- tropo_sif_data$Mean
@@ -36,7 +37,7 @@ nirv.col   <- "#00000025"
 pa.col     <- inf.cols[3]
 sza.col    <- inf.cols[7]
 vza.col    <- inf.cols[5]
-y_sif      <- c(0.2, 0.7)
+y_sif      <- c(0.3, 0.8)
 y_nirv     <- c(0.25, 0.33)
 y_lab_sif  <- bquote("SIF"[Daily]*" (mW/m"^"2"*"/sr/nm)")
 y_lab_nirv <- bquote("NIRv")
@@ -76,3 +77,30 @@ mtext(4, text = y_lab_nirv, line = 2, outer = FALSE)
 box()
 
 dev.off()
+
+
+summary(lm(tropo_sif_mean~gome2_sif_mean))
+summary(lm(tropo_sif_mean~oco2_sif_mean))
+summary(lm(tropo_sif_mean~oco3_sif_mean))
+summary(lm(oco2_sif_mean~oco3_sif_mean))
+summary(lm(oco2_sif_mean~gome2_sif_mean))
+summary(lm(oco3_sif_mean~gome2_sif_mean))
+
+
+df <- cbind(tropo_sif_mean, gome2_sif_mean, oco2_sif_mean, oco3_sif_mean)
+colnames(df) <- c("TROPO", "GOME-2", "OCO2", "OCO3")
+
+df_cor <- round(cor(df),  2)
+p.mat  <- cor.mtest(df)
+
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+
+corrplot(df_cor, method="color", col=col(40),  
+         order="original", cl.cex = 1,
+         addCoef.col = "black", # Add coefficient of correlation
+         tl.col="black", tl.srt=45, #Text label color and rotation
+         # Combine with significance
+         p.mat = p.mat, sig.level = 0.01, insig = "pch",
+         pch.cex = 6, pch.col = "#00000050",
+         diag=TRUE)
+
